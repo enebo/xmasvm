d1004fe4ac045382445122a2b6572662b0ac36bf
Thomas E. Enebo <tom.enebo@gmail.com>
2019-12-24 14:36:39 -0600

    Beginning of executable compiler.  The compiler itself only will generate an x86 nasm file which can then be compiled.  Overall, I am unhappy with how I organized things.  I wanted each Instruction to have trait impls per backend but somehow that ended up bubbling all the way back to generating a program list.  I should probably change the responsibility here so that the Interpreter and the Compiler will contain all the logic.  I liked a simple polymorphic loop but reversing the responsibility will make a much simpler implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate log;
 extern crate simple_logger;
 use log::debug;
 use crate::Terminate::{RanOffEnd, ProgramHalted};
+use std::fs;
+use std::borrow::BorrowMut;
 
 #[cfg(test)]
 mod tests {
@@ -12,6 +14,12 @@ mod tests {
     fn test_interpreter_halt() {
         let program: Vec<Box<dyn Instruction<Interpreter>>> = vec!(halt());
         Interpreter::new().execute(program).unwrap();
+    }
+
+    #[test]
+    fn test_compiler_halt() {
+        let program: Vec<Box<dyn Instruction<Compiler>>> = vec!(haltc());
+        Compiler::new().execute(program).unwrap();
     }
 
     #[test]
@@ -47,13 +55,17 @@ const REGISTERS_SIZE: usize = 32;
 type Registers = [usize; REGISTERS_SIZE];
 
 mod instruction_set {
-    use crate::{Instruction, HaltInstruction, IncrementInstruction, BranchNotEqualInstruction};
+    use crate::{Instruction, HaltInstruction, IncrementInstruction, BranchNotEqualInstruction, Compiler};
 
     pub fn branch_not_equal(test: usize, value: usize, jump: usize) -> Box<dyn Instruction>{
         Box::new(BranchNotEqualInstruction { test, value, jump })
     }
 
     pub fn halt() -> Box<dyn Instruction>{
+        Box::new(HaltInstruction {})
+    }
+
+    pub fn haltc() -> Box<dyn Instruction<Compiler>>{
         Box::new(HaltInstruction {})
     }
 
@@ -68,7 +80,7 @@ pub enum Terminate {
 }
 
 pub trait Instruction<Machine=Interpreter> {
-    fn process(&self, machine: Machine) -> Result<usize, Terminate>;
+    fn process(&self, machine: &mut Machine) -> Result<usize, Terminate>;
 }
 
 pub trait ProgramExecutor<Machine=Self> {
@@ -77,6 +89,25 @@ pub trait ProgramExecutor<Machine=Self> {
 
 #[derive(Debug, Clone)]
 pub struct Compiler {
+    program: String
+}
+
+impl Compiler {
+    fn new() -> Compiler {
+        Compiler {
+            program: String::new()
+        }
+    }
+
+    fn write_prologue(&mut self) {
+        self.program.push_str("global _start\n");
+        self.program.push_str("section .text\n");
+        self.program.push_str("_start:\n");
+    }
+
+    fn write_epilogue(&mut self) {
+        self.program.push_str(";;;;; Pushed with xmasvm...ho ho ho\n");
+    }
 }
 
 impl ProgramExecutor for Compiler {
@@ -84,17 +115,24 @@ impl ProgramExecutor for Compiler {
         // FIXME: init can only be called once to init so just ignore errors.  Ultimately, this should be passed in.
         match simple_logger::init() { _ => {} }
 
-        let mut ipc = 0;
+        self.write_prologue();
 
-        loop {
-            debug!("EXECUTING IPC {}", ipc);
-            if ipc >= program.len() { return Ok(()); }
+        let length = program.len();
 
-            match program[ipc].process(self.to_owned()) {
-                Ok(_) => { ipc += 1 },
+        for ipc  in 0..length {
+            debug!("EMITTING IPC {}", ipc);
+
+            match program[ipc].process(self) {
+                Ok(_) => {},
                 Err(err) => { return Err(err) }
             }
         }
+
+        self.write_epilogue();
+
+        fs::write("xmasvm.asm", &self.program).expect("Could not write asm file?");
+
+        Ok(())
     }
 }
 
@@ -122,7 +160,7 @@ impl ProgramExecutor for Interpreter {
             debug!("EXECUTING IPC {}", self.ipc);
             if self.ipc >= program.len() { return Err(RanOffEnd); }
 
-            match program[self.ipc].process(self.to_owned()) {
+            match program[self.ipc].process(self) {
                 Ok(new_ipc) => { self.ipc = new_ipc },
                 Err(ProgramHalted) => { return Ok(()) } // better way?
                 Err(err) => { return Err(err) }
@@ -135,14 +173,17 @@ impl ProgramExecutor for Interpreter {
 struct HaltInstruction {}
 
 impl Instruction<Interpreter> for HaltInstruction {
-    fn process(&self, _machine: Interpreter) -> Result<usize, Terminate> {
+    fn process(&self, _machine: &mut Interpreter) -> Result<usize, Terminate> {
         Err(ProgramHalted)
     }
 }
 
 impl Instruction<Compiler> for HaltInstruction {
-    fn process(&self, _machine: Compiler) -> Result<usize, Terminate> {
-        Err(ProgramHalted)
+    fn process(&self, machine: &mut Compiler) -> Result<usize, Terminate> {
+        machine.program.push_str("    mov eax, 1              ; exit()\n");
+        machine.program.push_str("    mov ebx, 0              ; status = 0\n");
+        machine.program.push_str("    int 0x80                ; call exit\n");
+        Ok(0)
     }
 }
 
@@ -152,7 +193,7 @@ struct IncrementInstruction {
 }
 
 impl Instruction<Interpreter> for IncrementInstruction {
-    fn process(&self, mut machine: Interpreter) -> Result<usize, Terminate> {
+    fn process(&self, mut machine: &mut Interpreter) -> Result<usize, Terminate> {
         let value = machine.registers[self.result];
         machine.registers[self.result] = value + 1;
         debug!("REGISTER {} is now {}", self.result, machine.registers[self.result]);
@@ -168,7 +209,7 @@ struct BranchNotEqualInstruction {
 }
 
 impl Instruction<Interpreter> for BranchNotEqualInstruction {
-    fn process(&self, machine: Interpreter) -> Result<usize, Terminate> {
+    fn process(&self, machine: &mut Interpreter) -> Result<usize, Terminate> {
         let test = machine.registers[self.test];
         let value = machine.registers[self.value];
         if test != value {
@@ -187,7 +228,7 @@ struct AddInstruction {
 }
 
 impl Instruction<Interpreter> for AddInstruction {
-    fn process(&self, mut machine: Interpreter) -> Result<usize, Terminate> {
+    fn process(&self, mut machine: &mut Interpreter) -> Result<usize, Terminate> {
         machine.registers[self.result] = machine.registers[self.operand1] + machine.registers[self.operand2];
         Ok(machine.ipc + 1)
     }
