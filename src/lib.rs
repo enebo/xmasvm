b8db8d34f1d9aed70faddbef29de93ed70a7b763
Thomas E. Enebo <tom.enebo@gmail.com>
2020-01-01 14:13:17 -0600

    Make helper macros direct! and literal! because rustfmt makes what I did as a 1-liner for specifying an operand as 4 lines.  I tried making 2 module functions in operand but my ownership of operands in instructions passed to the interpreter is apparently fragile (and I suspect poorly thought out).  Something bugged me with the idea of doing a Copy/Clone for everything but passing by reference forces multiple code levels lifetimes to line up.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,27 @@ mod instruction;
 mod interpreter;
 mod operand;
 
+// I tried making module fns for this but making the operand in that fn ended up
+// making these lets in the tests would have lost the struct by the time it was
+// used (e.g. let a = &operand::direct(1) -> Operand { tag: Direct, value: 1 }
+macro_rules! direct {
+    ($value:expr) => {{
+        &Operand {
+            tag: Tag::Direct,
+            value: $value,
+        }
+    }};
+}
+
+macro_rules! literal {
+    ($value:expr) => {{
+        &Operand {
+            tag: Tag::Literal,
+            value: $value,
+        }
+    }};
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,7 +35,6 @@ mod tests {
     use crate::instruction::instruction_set::*;
     use crate::instruction::Instruction;
     use crate::interpreter::Interpreter;
-    use crate::operand::Tag::{Direct, Literal};
     use crate::operand::{Operand, Tag};
 
     #[test]
@@ -31,10 +51,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_ran_off_end() {
-        let a = &Operand {
-            tag: Direct,
-            value: 0,
-        };
+        let a = direct!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![increment(a)];
         let result = Interpreter::new(program).execute();
 
@@ -45,11 +62,7 @@ mod tests {
     fn test_interpreter_invalid_store() {
         let program: &Vec<Box<dyn Instruction>> = &vec![halt()];
         let mut interpreter = Interpreter::new(program);
-        let value = &Operand {
-            tag: Literal,
-            value: 0,
-        };
-
+        let value = literal!(0);
         let result = interpreter.store(&value, 0);
 
         assert_eq!(result.err(), Some(Terminate::RegisterInvalid));
@@ -57,10 +70,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_store_invalid_result() {
-        let b = &Operand {
-            tag: Literal,
-            value: 0,
-        };
+        let b = literal!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![store(b, b), halt()];
         let result = Interpreter::new(program).execute();
 
@@ -69,10 +79,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_increment() {
-        let b = &Operand {
-            tag: Tag::Direct,
-            value: 0,
-        };
+        let b = direct!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![increment(b), increment(b), halt()];
         let mut interpreter = Interpreter::new(program);
 
@@ -87,10 +94,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_increment_invalid_result() {
-        let b = &Operand {
-            tag: Literal,
-            value: 0,
-        };
+        let b = literal!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![increment(b), increment(b), halt()];
         let result = Interpreter::new(program).execute();
 
@@ -99,10 +103,7 @@ mod tests {
 
     #[test]
     fn test_compiler_increment() {
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
+        let b = direct!(1);
         let program: &Vec<Box<dyn Instruction>> = &vec![increment(b), increment(b), halt()];
         let mut compiler = Compiler::new(program);
 
@@ -111,18 +112,9 @@ mod tests {
 
     #[test]
     fn test_interpreter_add() {
-        let a = &Operand {
-            tag: Direct,
-            value: 0,
-        };
-        let five = &Operand {
-            tag: Literal,
-            value: 5,
-        };
-        let one = &Operand {
-            tag: Literal,
-            value: 1,
-        };
+        let a = direct!(0);
+        let five = literal!(5);
+        let one = literal!(1);
         let program: &Vec<Box<dyn Instruction>> = &vec![add(a, five, one), halt()];
         let mut interpreter = Interpreter::new(program);
 
@@ -133,18 +125,9 @@ mod tests {
 
     #[test]
     fn test_compiler_add() {
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
-        let five = &Operand {
-            tag: Literal,
-            value: 5,
-        };
-        let one = &Operand {
-            tag: Literal,
-            value: 1,
-        };
+        let b = direct!(1);
+        let five = literal!(5);
+        let one = literal!(1);
         let program: &Vec<Box<dyn Instruction>> = &vec![add(b, five, one), halt()];
         let mut compiler = Compiler::new(program);
 
@@ -153,10 +136,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_add_invalid_result() {
-        let b = &Operand {
-            tag: Literal,
-            value: 0,
-        };
+        let b = literal!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![add(b, b, b), halt()];
         let result = Interpreter::new(program).execute();
 
@@ -165,14 +145,8 @@ mod tests {
 
     #[test]
     fn test_compiler_add_no_mov() {
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
-        let five = &Operand {
-            tag: Literal,
-            value: 5,
-        };
+        let b = direct!(1);
+        let five = literal!(5);
         let program: &Vec<Box<dyn Instruction>> = &vec![add(b, b, five), halt()];
         let mut compiler = Compiler::new(program);
 
@@ -181,18 +155,9 @@ mod tests {
 
     #[test]
     fn test_interpreter_branch_not_equal() {
-        let a = &Operand {
-            tag: Direct,
-            value: 0,
-        };
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
-        let five = &Operand {
-            tag: Literal,
-            value: 5,
-        };
+        let a = direct!(0);
+        let b = direct!(1);
+        let five = literal!(5);
         let program: &Vec<Box<dyn Instruction>> = &vec![
             store(a, five),
             increment(b),
@@ -204,14 +169,8 @@ mod tests {
 
     #[test]
     fn test_compiler_branch_not_equal() {
-        let a = &Operand {
-            tag: Direct,
-            value: 0,
-        };
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
+        let a = direct!(0);
+        let b = direct!(1);
         // Jump past second increment so halt reports 1 instead of 2.
         let program: &Vec<Box<dyn Instruction>> = &vec![
             increment(b),
@@ -226,14 +185,8 @@ mod tests {
 
     #[test]
     fn test_interpreter_push_pop() {
-        let a = &Operand {
-            tag: Direct,
-            value: 0,
-        };
-        let b = &Operand {
-            tag: Direct,
-            value: 1,
-        };
+        let a = direct!(0);
+        let b = direct!(1);
         let program: &Vec<Box<dyn Instruction>> = &vec![
             increment(a),
             push(a), // S: 1.
@@ -264,10 +217,7 @@ mod tests {
 
     #[test]
     fn test_interpreter_pop_invalid_result() {
-        let b = &Operand {
-            tag: Literal,
-            value: 0,
-        };
+        let b = literal!(0);
         let program: &Vec<Box<dyn Instruction>> = &vec![push(b), pop(b), halt()];
         let result = Interpreter::new(program).execute();
 
